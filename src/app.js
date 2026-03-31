37b888875d20fdf7fd84b95ca93298f96f3ce7a8
Marina <casaspm@uoc.edu>
2026-03-31 18:02:09 +0200

    funcions i estructura sobretot carrousel

--- src/app.js
+++ src/app.js
@@ -1,44 +1,36 @@
 import Swiper from "swiper";
 
 import { Navigation, Pagination } from "swiper/modules";
-//import "swiper/css";
-
-//import "swiper/css";
-//import "swiper/css/navigation";
-//import "swiper/css/pagination";
+/*import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";*/
+import "swiper/swiper-bundle.css";
 
 const swiper = new Swiper(".swiper", {
     modules: [Navigation, Pagination],
-    /*slidesPerView: 3,
-    spaceBetween: 0,
-    slidesPerGroup: 1,*/
+    slidesPerView: 1,
+    spaceBetween: 24,
+    slidesPerGroup: 1,
     loop: true,
-    centeredSlides: true,
-    /*effect: "cards",
-    cardsEffect: {
-        // ...
-    },*/
-    /*navigation: {
+    centeredSlides: false,
+
+    navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-    },*/
+    },
     /*pagination: {
         el: ".swiper-pagination",
         clickable: true,
         type: "bullets",
     },*/
     breakpoints: {
-        0: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
         768: {
             slidesPerView: 2,
-            spaceBetween: 30,
+            slidesPerGroup: 2,
         },
         1024: {
             slidesPerView: 3,
-            spaceBetween: 40,
+            slidesPerGroup: 3,
         },
     },
 });
