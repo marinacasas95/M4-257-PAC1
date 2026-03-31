90ee4f3073752ac888b52133c7c629c09349f526
Marina <casaspm@uoc.edu>
2026-03-31 14:48:51 +0200

    Estructura acabada

--- src/app.js
+++ src/app.js
@@ -1,2 +1,44 @@
-const name = "world";
-console.log(`Hello ${name}`);
+import Swiper from "swiper";
+
+import { Navigation, Pagination } from "swiper/modules";
+//import "swiper/css";
+
+//import "swiper/css";
+//import "swiper/css/navigation";
+//import "swiper/css/pagination";
+
+const swiper = new Swiper(".swiper", {
+    modules: [Navigation, Pagination],
+    /*slidesPerView: 3,
+    spaceBetween: 0,
+    slidesPerGroup: 1,*/
+    loop: true,
+    centeredSlides: true,
+    /*effect: "cards",
+    cardsEffect: {
+        // ...
+    },*/
+    /*navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },*/
+    /*pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        type: "bullets",
+    },*/
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+    },
+});
